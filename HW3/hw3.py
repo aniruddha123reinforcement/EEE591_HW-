5f2a4ba23caf9a19928f9f3040928a9f6d1a11f5
aniruddha123reinforcement <81187574+aniruddha123reinforcement@users.noreply.github.com>
2024-02-09 13:42:01 -0700

    Create hw3.py

---
+++ HW3/hw3.py
@@ -0,0 +1,97 @@
+################################################################################
+# Created on Fri Aug 24 13:36:53 2018                                          #
+#                                                                              #
+# @author: olhartin@asu.edu; updates by sdm                                    #
+#                                                                              #
+# Program to solve resister network with voltage and/or current sources        #
+################################################################################
+
+import numpy as np                     # needed for arrays
+from numpy.linalg import solve         # needed for matrices
+from read_netlist import read_netlist  # supplied function to read the netlist
+import comp_constants as COMP          # needed for the common constants
+
+# this is the list structure that we'll use to hold components:
+# [ Type, Name, i, j, Value ]
+
+################################################################################
+# How large a matrix is needed for netlist? This could have been calculated    #
+# at the same time as the netlist was read in but we'll do it here             #
+# Input:                                                                       #
+#   netlist: list of component lists                                           #
+# Outputs:                                                                     #
+#   node_cnt: number of nodes in the netlist                                   #
+#   volt_cnt: number of voltage sources in the netlist                         #
+################################################################################
+
+def get_dimensions(netlist):           # pass in the netlist
+
+    ### EXTRA STUFF HERE!
+
+#    print(' Nodes ', node_cnt, ' Voltage sources ', volt_cnt)
+    return node_cnt,volt_cnt
+
+################################################################################
+# Function to stamp the components into the netlist                            #
+# Input:                                                                       #
+#   y_add:    the admittance matrix                                            #
+#   netlist:  list of component lists                                          #
+#   currents: the matrix of currents                                           #
+#   node_cnt: the number of nodes in the netlist                               #
+# Outputs:                                                                     #
+#   node_cnt: the number of rows in the admittance matrix                      #
+################################################################################
+
+def stamper(y_add,netlist,currents,node_cnt):
+    # return the total number of rows in the matrix for
+    # error checking purposes
+    # add 1 for each voltage source...
+
+    for comp in netlist:                  # for each component...
+        #print(' comp ', comp)            # which one are we handling...
+
+        # extract the i,j and fill in the matrix...
+        # subtract 1 since node 0 is GND and it isn't included in the matrix
+        i = comp[COMP.I] - 1
+        j = comp[COMP.J] - 1
+
+        if ( comp[COMP.TYPE] == COMP.R ):           # a resistor
+            if (i >= 0):                            # add on the diagonal
+                y_add[i,i] += 1.0/comp[COMP.VAL]
+            
+            #EXTRA STUFF HERE!
+
+    return node_cnt  # should be same as number of rows!
+
+################################################################################
+# Start the main program now...                                                #
+################################################################################
+
+# Read the netlist!
+netlist = read_netlist()
+"""
+# Print the netlist so we can verify we've read it correctly
+for index in range(len(netlist)):
+    print(netlist[index])
+print("\n")
+""" 
+
+# Get the count of nodes and voltage sources and print them
+nodes_count, voltage_sources_count = get_dimensions(netlist)
+
+# Get the total number of nodes
+total = nodes_count + voltage_sources_count
+
+# Prepare admittance, currents, voltage lists
+admittance = np.zeros((total, total), dtype=float)
+currents = np.zeros(total, dtype=float)
+voltages = np.zeros(total, dtype=float)
+
+# Call stamper function to return number of nodes
+node_count = stamper(admittance, netlist, currents, nodes_count)
+# print("Nodes count from stamper function =", node_count)
+
+# Solve for voltages
+voltages = solve(admittance, currents)
+print(voltages)
+#EXTRA STUFF HERE!
